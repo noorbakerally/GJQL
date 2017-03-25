5389ba6100fe91f048937a941742191299c8bcff
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-03-25 08:33:30 +0100

    test combined

--- src/test/java/TestQuery.java
+++ src/test/java/TestQuery.java
@@ -1,15 +1,15 @@
 /**
  * Created by bakerally on 3/17/17.
  */
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import fr.opensensingcity.GJQL.GraphUtils;
 import fr.opensensingcity.GJQL.factory.MappingFactory;
 import fr.opensensingcity.GJQL.factory.QResourceFactory;
-import fr.opensensingcity.GJQL.qresource.QResource;
 import fr.opensensingcity.GJQL.mapping.Mapping;
+import fr.opensensingcity.GJQL.qresource.QResource;
 import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.sparql.algebra.Op;
@@ -17,32 +17,42 @@ import org.apache.jena.sparql.algebra.OpAsQuery;
 import org.apache.jena.sparql.algebra.op.OpBGP;
 import org.apache.jena.sparql.core.BasicPattern;
 import org.junit.Test;
-import testutils.TestUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class TestQuery11 {
+public class TestQuery {
     String message = "Robert";
 
 
     @Test
     public void testGenerateSPARQLFromJson() throws IOException, URISyntaxException {
-        JsonParser parser = new JsonParser();
+        assertTrue(performithTest(11));
+    }
 
+    public boolean performithTest(int ithTest) throws IOException, URISyntaxException {
+
+        //get absolute path to test folder
+        URL location = TestQuery.class.getProtectionDomain().getCodeSource().getLocation();
+        String path = location.getPath();
+        String testClassPath = path.substring(0, path.lastIndexOf("/"))+"/TestQuery"+ithTest;
+
+        JsonParser parser = new JsonParser();
         //get resource representation
-        String queryJSON = TestUtils.getFileContentFromResource(this,"query.json");
+        String queryJSON = getFileContentFromResource(ithTest,"query.json");
         JsonObject queryObject = parser.parse(queryJSON).getAsJsonObject();
         QResource resource = QResourceFactory.
                 loadSimpleQResourceFromJSON(queryObject);
 
         //get mapping representation
-        String queryMappings = TestUtils.getFileContentFromResource(this,"mappings.json");
+        String queryMappings = getFileContentFromResource(ithTest,"mappings.json");
         JsonObject queryMappingObject = parser.parse(queryMappings).getAsJsonObject();
         Mapping simpleMapping = MappingFactory.generateSimpleMappingFromJSON(queryMappingObject);
 
@@ -55,14 +65,8 @@ public class TestQuery11 {
             query.setPrefix(prefix,simpleMapping.getPrefixes().get(prefix));
         }
 
-
-
-        String oqueryStr  = TestUtils.getFileContentFromResource(this,"query.rq");
-
-        //assertTrue(originalQuery.getQueryPattern().equalTo(query.getQueryPattern(),null));
-
         //generate results from query
-        String modelIRI = getClass().getResource("/"+getClass().getSimpleName()).toString()+"/graph.ttl";
+        String modelIRI = testClassPath+"/graph.ttl";
         ResultSet resultBindings = GraphUtils.executeSPARQL(query, modelIRI);
         List<QuerySolution> querySolutions = new ArrayList<QuerySolution>();
         while (resultBindings.hasNext()){
@@ -75,13 +79,25 @@ public class TestQuery11 {
         JsonObject generatedResultObject = parser.parse(result).getAsJsonObject();
 
         //load original result
-        String originalResult = TestUtils.getFileContentFromResource(this,"result.json");
+        String originalResult = getFileContentFromResource(ithTest,"result.json");
         JsonObject originalResultObject = parser.parse(originalResult).getAsJsonObject();
 
-       /* System.out.println(generatedResultObject);
-        System.out.println(originalResultObject);*/
 
+        return (generatedResultObject.equals(originalResultObject));
+    }
 
-        assertTrue(generatedResultObject.equals(originalResultObject));
+    public static String getFileContentFromResource(int ithTest,String filename) throws URISyntaxException, IOException {
+        //get absolute path to test folder
+        URL location = TestQuery.class.getProtectionDomain().getCodeSource().getLocation();
+        String path = location.getPath();
+        String testClassPath = path.substring(0, path.lastIndexOf("/"))+"/TestQuery"+ithTest;
+
+        File file = new File(testClassPath+"/"+filename);
+        FileInputStream fis = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fis.read(data);
+        fis.close();
+        String str = new String(data);
+        return str;
     }
 }
